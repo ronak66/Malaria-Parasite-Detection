d8ffeb102a76a415454900ec5a9478851ad2800c
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2019-11-12 07:49:32 +0530

    adding contour feature to ml model

--- src/data_handling.py
+++ src/data_handling.py
@@ -21,3 +21,16 @@ class DataHandling:
         for index, row in df.iterrows():
             x.append(read_image(row["FILE"]))
         return x
+
+    def load_dataset(path, classes, size):
+        class_images = []
+        for cls in classes :
+            cls_imgs = []
+            img_names = os.listdir(path + cls + "/")
+            batch_size=0
+            for img_name in img_names :
+                if(batch_size==size): break
+                cls_imgs.append(cv2.resize(cv2.imread(path + cls + "/" + img_name), (300, 200)))
+                batch_size+=1
+            class_images.append(np.array(cls_imgs))
+        return class_images
